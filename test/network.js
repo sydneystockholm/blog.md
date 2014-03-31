a8f34b374febb4eacf35ca352ffc20c46bde0976
Chris O'Hara <cohara87@gmail.com>
2014-03-31 12:11:00 +1100

    Select network posts orderly randomly

--- test/network.js
+++ test/network.js
@@ -192,5 +192,24 @@ describe('Network', function () {
         });
     });
 
+    it('should select posts aggregated from all blogs, sorted randomly', function (done) {
+        var network = new Network();
+        network.add('fooblog', new Blog([
+            { id: 1, title: 'foo', date: '2012-10-01' }
+          , { id: 2, title: 'foo', date: '2012-10-03' }
+          , { id: 3, title: 'bar', date: '2012-10-05' }
+        ]));
+        network.add('barblog', new Blog([
+            { id: 1, title: 'a', date: '2012-10-02' }
+          , { id: 2, title: 'b', date: '2012-10-04' }
+          , { id: 3, title: 'c', date: '2012-10-06' }
+        ]));
+        network.on('load', function () {
+            var posts = network.select({ random: true });
+            assert.equal(posts.length, 6);
+            done();
+        });
+    });
+
 });
 
